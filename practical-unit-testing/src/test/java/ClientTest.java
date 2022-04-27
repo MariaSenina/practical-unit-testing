8c84a42cccf93abeb3c88358cad3bc6a1eb46131
Maria <maria.senina15@gmail.com>
2022-04-27 14:41:31 -0400

    Text Fixtrure creation with annotations

---
+++ practical-unit-testing/src/test/java/ClientTest.java
@@ -0,0 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ClientTest {
+    private Address addressA;
+    private Address addressB;
+    private Client client;
+
+    // Make JUnit execute the following method before each test method is executed
+    @Before
+    public void setUp() {
+        addressA = new Address("street A");
+        addressB = new Address("street B");
+        client = new Client();
+    }
+
+    /*
+    Methods annotated with @After are executed after each test method
+    @BeforeClass and @AfterClass annotations work in the same way as @Before and @After but on a class level.
+        ^ usually used to create costly resources and clean them afterwards.
+     */
+
+    @Test
+    public void afterCreationShouldHaveNoAddress() {
+        assertEquals(0, client.getAddresses().size());
+    }
+    @Test
+    public void shouldAllowToAddAddress() {
+        client.addAddress(addressA);
+        assertEquals(1, client.getAddresses().size());
+        assertTrue(client.getAddresses().contains(addressA));
+    }
+    @Test
+    public void shouldAllowToAddManyAddresses() {
+        client.addAddress(addressA);
+        client.addAddress(addressB);
+        assertEquals(2, client.getAddresses().size());
+        assertTrue(client.getAddresses().contains(addressA));
+        assertTrue(client.getAddresses().contains(addressB));
+    }
+}
